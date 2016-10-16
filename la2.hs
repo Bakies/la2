a1a556076c6623124da61dd3b7e3277367a4ee25
Jon Bakies <jon@baki.es>
2016-10-16 13:11:18 -0400

    Implemented addpoly

--- la2.hs
+++ la2.hs
@@ -5,12 +5,22 @@ type Exp=Int
 type Polynomial=[(Coeff, Exp)] -- a list of terms in a polynomial formula
 
 
--- TODO: implement addpoly to add two polynomials
+-- call looks like this: addpoly [(3, 2), (4, 1)] [(2, 3), (6, 2)] for (3x^2 + 4x) + (2x^3 + 6x^2)
 addpoly::Polynomial->Polynomial->Polynomial
-
-
+-- return if either poly is empty
+addpoly xs [] = xs
+addpoly [] ys = ys
+addpoly ((i, j):xs) ((k, l):ys)
+-- if the two exponents are equal, add the coef
+							-- advance both and prepend the addition
+    | j == l = ((i + k, j):(addpoly xs ys))
+-- if the expression in xs is higher dont advance ys, prepend xs to the answer 
+-- since there is nothing to add to it
+    | j > l  = (i, j):(addpoly xs ((k, l):ys))
+-- vice versa for ys
+    | l > j  = (k, l):(addpoly ((i, j):xs) ys) 
 
 -- TODO: implement multpoly to multiply two polynomials
-multpoly::Polynomial->Polynomial->Polynomial
+-- multpoly::Polynomial->Polynomial->Polynomial
 
 
